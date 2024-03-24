569118dd370682a541a9602bdf7fe8d2f3c8f18b
Sopu Matei <sopumatei@gmail.com>
2024-03-24 19:50:12 +0200

    Make the application responsive

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('side-bar');
-const margin = '-40vw';
+const margin = '-60vw';
+const closeBtn = document.querySelector('.closeBtn');
 let open = false;
 
 menuBtn.addEventListener('click', () => {
-    if(!open) {
-        open = true
-        menu.style.marginLeft = 0;
-    }
-    else {
-        open = false;
-        menu.style.marginLeft = margin;
-    }
+    menu.style.marginLeft = 0;
 })
+
+closeBtn.addEventListener('click', () => {
+    menu.style.marginLeft = margin;
+})
+
+// Custom mobile height
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
